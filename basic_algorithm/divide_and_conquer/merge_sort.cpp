dc7a3d83ea8b84b64633554d889881d49878287a
liuyuandong <liuyuanmudong@gmail.com>
2017-03-13 17:59:03 +0800

    some instance of divide and conquer algorithm

---
+++ basic_algorithm/divide_and_conquer/merge_sort.cpp
@@ -0,0 +1,31 @@
+void merge(int []a, int left, int middle, int right, int []temp)
+{
+	int i = left, j = middle + 1;
+	int k = 0;
+	while(i <= middle && j <= right )
+		if(a[i] <= a[j])
+			temp[k++] = a[i++];
+		else
+			temp[k++] = a[j++];
+	while(i <= middle)
+		temp[k++] = a[i++];
+	while(j <= right)
+		temp[k++] = a[j++];
+
+	// copy back
+	for(i = 0; i < k; i++)
+		a[left + i] = temp[i];
+}
+
+void merge_sort(int[] val, int left, int right, int[] temp)
+{
+	if(left == right)         // base case: only one element == sorted
+		return;
+
+	int middle = (right -left) / 2;         // divide
+
+	merge_sort(val, left, middle, temp);        // conquer left
+	merge_sort(val, middle+1, right, temp);     // conquer right
+
+	merge(val, left, middle, right, temp);      // combine
+}
